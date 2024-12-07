22820ab7536a45eef6a37414028b1d8c6d264403
Tom Aylott <oblivious@subtlegradient.com>
2024-12-07 17:41:27 -0500

    so pretty

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -16,7 +16,8 @@ export function Card({
       className={className}
       href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
       rel="noopener noreferrer"
-      target="_blank">
+      target="_blank"
+    >
       <h2>
         {title} <span>-&gt;</span>
       </h2>
