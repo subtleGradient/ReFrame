aaaeaf9ee2987cf03e74d37291c6a20f636942c2
Tom Aylott <oblivious@subtlegradient.com>
2024-12-07 21:05:00 -0500

    cleanup

--- packages/reframe-bridge/StreamingFragment.tsx
+++ packages/reframe-bridge/StreamingFragment.tsx
@@ -1,31 +1,28 @@
-import React from "react"
+"use client"
+
+import React, { ReactElement, ReactNode } from "react"
 import { useDeferredValue, useEffect, useState } from "react"
 import { AsyncIterable$forEach } from "./AsyncIterable$forEach"
 
-export function StreamingFragment({
-  initialChildren,
-  children: childStream,
-}: {
-  initialChildren?: React.ReactElement
-  children: AsyncIterable<
-    | React.ReactElement //
-    | { append?: undefined; replace: React.ReactNode }
-    | { replace?: undefined; append: React.ReactNode }
-  >
-}) {
-  const [children, setChildren] = useState<React.ReactNode[]>([])
+interface StreamingFragmentProps {
+  initialChildren?: ReactElement
+  children: AsyncIterable<ReactElement | { replace: ReactNode }>
+}
+
+export function StreamingFragment({ initialChildren, children: stream }: StreamingFragmentProps) {
+  const [children, setChildren] = useState<ReactNode[]>([])
 
   useEffect(() => setChildren([initialChildren]), [initialChildren])
 
   useEffect(() => {
     const control = new AbortController()
-    AsyncIterable$forEach(childStream, control.signal, (child) => {
+    AsyncIterable$forEach(stream, control.signal, (child) => {
+      if (control.signal.aborted) return
       if ("replace" in child) return setChildren([child.replace])
-      if ("append" in child) return setChildren((children) => [...children, child.append])
       setChildren((children) => [...children, child])
     })
     return () => control.abort()
-  }, [childStream])
+  }, [stream])
 
-  return <>{useDeferredValue(Object.values(children))}</>
+  return <>{useDeferredValue(children)}</>
 }
