7302592121ee01f930ba765dd4ee329181642cc2
Tom Aylott <oblivious@subtlegradient.com>
2024-12-07 21:37:40 -0500

    refactor AsyncIterable$forEach

--- packages/reframe-bridge/AsyncIterable$forEach.tsx
+++ packages/reframe-bridge/AsyncIterable$forEach.tsx
@@ -1,6 +1,7 @@
 import { MaybePromise } from "./types"
 
-export async function AsyncIterable$forEach<T>(
+/** @deprecated -- @see AsyncIterable$forEach */
+export async function AsyncIterable$forEach_old<T>(
   it: AsyncIterable<T>,
   signal: AbortSignal,
   drain: (item: T, index: number, items: typeof it) => MaybePromise<unknown>,
@@ -10,5 +11,23 @@ export async function AsyncIterable$forEach<T>(
     index++
     if (signal.aborted) return
     await drain(item, index, it)
+  }
+}
+
+export async function AsyncIterable$forEach<T>(
+  them: AsyncIterable<T>,
+  forEach: (item: T, index: number, items: typeof them) => MaybePromise<unknown>,
+  signal?: AbortSignal,
+) {
+  let index = -1
+  try {
+    signal?.throwIfAborted()
+    for await (const item of them) {
+      signal?.throwIfAborted()
+      await forEach(item, index++, them)
+    }
+  } catch (error) {
+    if (error instanceof Error && error.name === "AbortError") return
+    throw error
   }
 }
