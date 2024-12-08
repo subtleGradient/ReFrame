e2c03af4281ca255c3c7c04986ea20e8642f8671
Tom Aylott <oblivious@subtlegradient.com>
2024-12-07 21:05:13 -0500

    dark mode +

--- demos/reframe-expo-demo-1/app/(tabs)/text-stream.tsx
+++ demos/reframe-expo-demo-1/app/(tabs)/text-stream.tsx
@@ -1,35 +1,39 @@
-import { ScrollView, Text, View } from "react-native"
+import { ThemedText } from "@/components/ThemedText"
+import { StreamingFragment } from "@sublegradient/reframe-bridge/StreamingFragment"
 import { fetch } from "expo/fetch"
 import React from "react"
-import { StreamingFragment } from "@sublegradient/reframe-bridge/StreamingFragment"
+import { ScrollView } from "react-native"
 
 export default function HomeScreen() {
   return (
     <ScrollView style={{ padding: 16 }}>
-      <Text style={{ fontSize: 24 }}>Streaming text demo (replace)</Text>
-      <Text style={{ fontSize: 14 }}>real streaming and chunked rendering</Text>
-      <Text>
+      <ThemedText style={{ fontSize: 24 }}>Streaming text demo (replace)</ThemedText>
+      <ThemedText style={{ fontSize: 14 }}>real streaming and chunked rendering</ThemedText>
+      <ThemedText>
         <StreamingFragment
-          initialChildren={<Text>loading...</Text>}
+          initialChildren={<ThemedText>loading...</ThemedText>}
           children={renderChunkedTimestamps}
         />
-      </Text>
+      </ThemedText>
 
-      <Text style={{ fontSize: 24 }}>Streaming text demo (append)</Text>
-      <Text style={{ fontSize: 14 }}>fake streaming, but real chunked rendering</Text>
-      <Text>
-        <StreamingFragment initialChildren={<Text>loading...</Text>} children={renderChunked} />
-      </Text>
+      <ThemedText style={{ fontSize: 24 }}>Streaming text demo (append)</ThemedText>
+      <ThemedText style={{ fontSize: 14 }}>fake streaming, but real chunked rendering</ThemedText>
+      <ThemedText>
+        <StreamingFragment
+          initialChildren={<ThemedText>loading...</ThemedText>}
+          children={renderChunked}
+        />
+      </ThemedText>
     </ScrollView>
   )
 }
 
 const renderChunked = {
   async *[Symbol.asyncIterator]() {
-    yield { replace: <Text>loading...</Text> }
+    yield { replace: <ThemedText>loading...</ThemedText> }
 
     const response = await fetch("https://baconipsum.com/api/?type=meat-and-filler")
-    yield { replace: <Text>response: {response.status}</Text> }
+    yield { replace: <ThemedText>response: {response.status}</ThemedText> }
 
     await new Promise((resolve) => setTimeout(resolve, 333))
     const text = await response.text()
@@ -40,38 +44,38 @@ const renderChunked = {
     for (const chunk of chunks) {
       index++
       if (chunk === `","`) {
-        yield <Text>{"\n\n"}</Text>
+        yield <ThemedText>{"\n\n"}</ThemedText>
         continue
       }
       if (index === 0) yield { replace: null }
-      yield <Text>{chunk}</Text>
+      yield <ThemedText>{chunk}</ThemedText>
       await new Promise((resolve) => setTimeout(resolve, 111 * Math.random()))
     }
 
-    yield <Text>{"\n\n"}Done</Text>
+    yield <ThemedText>{"\n\n"}Done</ThemedText>
   },
 }
 
 const renderChunkedTimestamps = {
   async *[Symbol.asyncIterator]() {
-    yield { replace: <Text>loading...</Text> }
+    yield { replace: <ThemedText>loading...</ThemedText> }
 
     const response = await fetch("http://localhost:8000")
-    yield { replace: <Text>response: {response.status}</Text> }
+    yield { replace: <ThemedText>response: {response.status}</ThemedText> }
 
     await new Promise((resolve) => setTimeout(resolve, 333))
 
     const { body } = response
 
     if (!body) {
-      yield <Text>ERROR: no body</Text>
+      yield <ThemedText>ERROR: no body</ThemedText>
       return
     }
     for await (const chunk of body) {
       const random = Math.random()
       yield {
         replace: (
-          <Text
+          <ThemedText
             style={{
               // random background color so it's easier to see that something changed
               backgroundColor: `hsl(${random * 360}, 50%, 50%)`,
@@ -80,11 +84,11 @@ const renderChunkedTimestamps = {
             }}
           >
             {new TextDecoder().decode(chunk)}
-          </Text>
+          </ThemedText>
         ),
       }
     }
 
-    yield <Text>{"\n\n"}Done</Text>
+    yield <ThemedText>{"\n\n"}Done</ThemedText>
   },
 }
