369bfcc6489ad862da5a0c5f864d9f7848610f05
Tom Aylott <oblivious@subtlegradient.com>
2024-12-09 18:29:52 -0500

    feat: add Promise_fromThenable utility function for handling thenable objects

---
+++ packages/reframe/random/Promise_fromThenable.tsx
@@ -0,0 +1,9 @@
+import type { Thenable } from "shared/ReactTypes"
+
+export function Promise_fromThenable<T>(thenable: Thenable<T>): Promise<T> {
+  if (thenable instanceof Promise) return thenable
+  if ("promise" in thenable && thenable.promise instanceof Promise) return thenable.promise
+  return ((thenable as any).promise = new Promise<T>((resolve, reject) =>
+    thenable.then(resolve, reject),
+  ))
+}
