9bcf8b1ee6c4b06d3fa35e18073f4a8837fcd8e6
Tom Aylott <oblivious@subtlegradient.com>
2024-12-09 21:40:20 -0500

    feat: extend ReFrameClient to support additional response properties in client configuration

--- packages/reframe/client/ReFrameClient.tsx
+++ packages/reframe/client/ReFrameClient.tsx
@@ -2,7 +2,7 @@ import ReactFlightClient from "@double-observer/react-client/flight"
 import { ReactFlightClientConfig } from "@double-observer/react-client/src/ReactFlightClientConfig"
 import { ChunkSource$forEach } from "../random/ChunkSource$forEach"
 import { Promise_fromThenable } from "../random/Promise_fromThenable"
-import { RSCSource } from "../random/types"
+import { FlightResponseProps, RSCSource } from "../random/types"
 import { createFlightResponse } from "./createFlightResponse"
 import { createClientConfig } from "./ClientConfig"
 
@@ -12,16 +12,26 @@ interface FromProps {
   onClose?(): void
   onError?(error: Error): typeof IGNORE_ERROR | void
   signal?: AbortSignal
+  responseProps?: Partial<FlightResponseProps>
+}
+
+type CreateClientConfigProps = Parameters<typeof createClientConfig>[0]
+
+interface ReFrameClientProps extends CreateClientConfigProps {
+  responseProps?: Partial<FlightResponseProps>
 }
 
 export default class ReFrameClient {
-  static create<T>(...args: Parameters<typeof createClientConfig>) {
-    return new ReFrameClient(createClientConfig(...args))
+  static create<T>({ responseProps, ...props }: ReFrameClientProps) {
+    const reframe = new ReFrameClient(createClientConfig(props))
+    reframe.responseProps = responseProps
+    return reframe
   }
+
+  private responseProps?: Partial<FlightResponseProps>
 
   constructor(public readonly config: ReactFlightClientConfig) {
     const flight = new ReactFlightClient(this.config) // satisfies IReactFlightClient
-    flight
     this.flight = flight
   }
 
@@ -39,6 +49,8 @@ export default class ReFrameClient {
           console.warn("moduleLoading.load called unexpectedly", "url", url, "nonce", nonce)
         },
       },
+      ...this.responseProps,
+      ...props?.responseProps,
     })
 
     void ChunkSource$forEach(
