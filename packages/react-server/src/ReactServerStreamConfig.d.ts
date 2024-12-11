298e5f905ce77e9212055cda64484f2fd0901f1c
Tom Aylott <oblivious@subtlegradient.com>
2024-12-11 15:59:03 -0500

    cleanup

--- packages/react-server/src/ReactServerStreamConfig.d.ts
+++ packages/react-server/src/ReactServerStreamConfig.d.ts
@@ -1,5 +1,1 @@
-export type Destination = unknown
-export type Chunk = unknown
-export type PrecomputedChunk = unknown
-
 export default interface ReactServerStreamConfig<Destination, Chunk, PrecomputedChunk> {}
