b36ed6ee9915928023226b5f361d2259a0a3c210
Tom Aylott <oblivious@subtlegradient.com>
2024-12-14 19:05:41 -0500

    Update rsc.tsx

--- demos/server/node/src/rsc.tsx
+++ demos/server/node/src/rsc.tsx
@@ -1,6 +1,6 @@
 import { ReFrameServer } from "@subtlegradient/reframe/server"
 import { Request, RequestHandler, Response } from "express"
-import React from "react"
+import React, { ComponentType } from "react"
 
 export interface ServerPageProps {
   signal: AbortSignal
@@ -13,54 +13,59 @@ export interface ServerPageProps {
   next?: never
 }
 
+const moduleBasePath = new URL("./", import.meta.url).href
+
 export function RSC$createRequestHandler(RootComponent: React.ComponentType<ServerPageProps>): RequestHandler {
   return async (req, res) => {
     res.setHeader("Transfer-Encoding", "chunked")
     // res.setHeader("Content-Type", "text/x-component") // the correct content type for streaming RSC
-    res.setHeader("Content-Type", "text/event-stream") // makes cloudflare stream the response
+    res.setHeader("Content-Type", "text/event-stream") // but this makes cloudflare stream the response
     res.setHeader("Connection", "keep-alive")
 
-    const abortController = new AbortController()
-
-    req.on("close", () => {
-      abortController.abort()
-    })
-
-    function Render() {
-      return (
-        <RootComponent
-          signal={abortController.signal}
-          params={req.query}
-          setHeader={(key, value) => {
-            if (key === "Content-Type") throw new Error("Cannot set Content-Type or else it'll break streaming")
-            return res.setHeader(key, value)
-          }}
-        />
-      )
-    }
-
-    const moduleBasePath = new URL("./", import.meta.url).href
-
     try {
-      const rscStream = await ReFrameServer.renderToPipeableStream(<Render />, moduleBasePath, {
+      const root = <Render {...{ RootComponent, req, res }} />
+
+      const rscStream = await ReFrameServer.renderToPipeableStream(root, moduleBasePath, {
         environmentName: "Server",
+        identifierPrefix: "ReFrameServer",
         onError(error) {
           console.error("ReFrameServer onError", error)
-          // Only end the response on error if it hasn't been sent yet
-          if (!res.writableEnded) {
-            res.end()
-          }
+          if (!res.writableEnded) res.end()
         },
         onPostpone(reason) {
           console.error("ReFrameServer onPostpone", reason)
         },
       })
+
       rscStream.pipe(res)
     } catch (error) {
       console.error("Stream error:", error)
-      if (!res.writableEnded) {
-        res.end()
-      }
+      if (!res.writableEnded) res.end()
     }
   }
+}
+
+interface RenderProps {
+  RootComponent: ComponentType<ServerPageProps>
+  req: Request
+  res: Response
+}
+
+function Render({ RootComponent, req, res }: RenderProps) {
+  const abortController = new AbortController()
+
+  req.on("close", () => {
+    abortController.abort()
+  })
+
+  return (
+    <RootComponent
+      signal={abortController.signal}
+      params={req.query}
+      setHeader={(key, value) => {
+        if (key === "Content-Type") throw new Error("Cannot set Content-Type or else it'll break streaming")
+        return res.setHeader(key, value)
+      }}
+    />
+  )
 }
