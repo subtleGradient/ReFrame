abee65cbb3602f1df9c3227fa06019f6ace7c479
Tom Aylott <oblivious@subtlegradient.com>
2024-12-14 20:39:39 -0500

    Update pod.sh

--- sh/pod.sh
+++ sh/pod.sh
@@ -5,9 +5,18 @@ cd "$(dirname "$0")" || exit
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 cd "$REPO_ROOT" || exit
 
+# podman machine config
+POD_NAME="podman-machine"
+POD_IMAGE="fedora:latest"
+POD_CPUS=2
+POD_MEMORY=2048
+POD_DISK_G=20
+
 if ! podman machine list | grep -q "Currently running"; then
-  podman machine init
+  podman machine init --name "$POD_NAME" --image "$POD_IMAGE" --cpus "$POD_CPUS" --memory "$POD_MEMORY" --disk "$POD_DISK_G"
   podman machine start
 fi
 
+podman ps
 podman machine list
+podman machine inspect
